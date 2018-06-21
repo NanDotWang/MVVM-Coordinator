56fe79087886b7485e352cc6b2f31e83a765e912
Nan Wang <nan.wang@klarna.com>
2018-06-21 13:09:13 +0200

    Initial Commit

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by Nan Wang on 2018-06-21.
+//  Copyright © 2018 Nan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
